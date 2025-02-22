a72bb463593f2ab4ef194d27d3acb86d32e3ca03
danielemporia <xie476@purdue.edu>
2025-02-22 12:54:39 -0500

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "remote.ai",
+  title: "pr",
   description: "ai interview prep",
 };
 
